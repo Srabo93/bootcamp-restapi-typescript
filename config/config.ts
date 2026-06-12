9d904b89d633970cfee372cb0ed70baf0b2d4a61
Armin Srabovic <a.s-dev@web.de>
2026-06-13 01:21:46 +0200

    throwing out outdated code

--- config/config.ts
+++ config/config.ts
@@ -1,14 +1,3 @@
-import * as dotenv from "dotenv";
-import fs from "fs";
-
-if (fs.existsSync(`${__dirname}/.env`)) {
-  console.log("Using .env file to supply config environment variables");
-  dotenv.config({ path: `${__dirname}/.env` });
-} else {
-  console.log("Using .env.example file to supply config environment variables");
-  dotenv.config({ path: `${__dirname}/.env` });
-}
-
 export const MONGODB_URI = process.env.MONGODB_URI as string;
 export const GEOCODER_API_KEY = process.env.GEOCODER_API_KEY as string;
 export const JWT_EXPIRE = process.env.JWT_EXPIRE as string;
